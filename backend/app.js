169db9df4ff041833ba83b103394a0859ce1f94b
asayenju <ashwinsayenju@gmail.com>
2024-06-26 01:00:59 +0700

    Database logic connection made

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then (() => {
-    console.log('our db is connected')
-}).catch(err => console.log(err.message))
+require('./models/db')
 const app = express()
 
 app.get('/', (req, res) => {
