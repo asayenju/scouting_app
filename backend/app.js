0487225496dd75a323b2e2b28a86fd2e72ffa9a4
asayenju <ashwinsayenju@gmail.com>
2024-06-25 21:08:23 +0700

    Successfully connected to mongodb database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then (() => {
+    console.log('our db is connected')
+}).catch(err => console.log(err.message))
 const app = express()
 
 app.get('/', (req, res) => {
